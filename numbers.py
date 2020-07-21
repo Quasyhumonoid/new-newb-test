3c600a7a888987e4de1aa851bed646057848d3bd
Quasyhumonoid <jpkirner@outlook.com>
2020-07-20 22:43:42 -0500

    modified Python notes

--- numbers.py
+++ numbers.py
@@ -1,6 +1,7 @@
 
 print(2 + 4 * 5)
 print(10 % 3)
+print(pow(3, 2))
 num = 5
 num = str(num)
 print(num[0])
@@ -10,7 +11,6 @@ print(num)
 num2 = (max(4, 6) + min(4, 6) + .6)
 print(num2)
 print(round(num2))
-
 from math import *
 print(floor(num2))
 print(ceil(num2))
