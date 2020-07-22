315ed744c013109158d0f4cbc9806a9a16055906
Quasyhumonoid <jpkirner@outlook.com>
2020-07-21 19:21:04 -0500

    updated calculator

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-num1 = int(input("enter the number you want to add too:"))
-num2 = int(input("enter the number you want to add too it:"))
+num1 = float(input("enter the number you want to add too:"))
+num2 = float(input("enter the number you want to add too it:"))
 num3 = num1 + num2
 print(num3)
