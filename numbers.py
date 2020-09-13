be95dbe68d0a77b81752c9638548055b33712635
Jonathan Kirner <jpkirner@outlook.com>
2020-09-13 16:27:02 -0500

    added comments to make more clear

--- numbers.py
+++ numbers.py
@@ -1,12 +1,12 @@
 
-print(2 + 4 * 5)
-print(10 % 3)
-print(pow(3, 2))
+print(2 + 4 * 5) #follows order of operations
+print(10 % 3) #prints remainder of the derivation
+print(pow(3, 2)) #3 to the 2nd power
 num = 5
-num = str(num)
+num = str(num) #converts variable to string variable
 print(num[0])
 num = -5
-num = abs(num)
+num = abs(num) #absolute value
 print(num)
 num2 = (max(4, 6) + min(4, 6) + .6)
 print(num2)
