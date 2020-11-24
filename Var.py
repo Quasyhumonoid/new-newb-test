58cab7764818f26b2eb4d0944f7c2a1fed6bf7da
Jonathan Kirner <jkirner@mechdyne.com>
2020-11-23 19:04:21 -0600

    notes on variables

--- Var.py
+++ Var.py
@@ -20,5 +20,5 @@ print(num + age) #float and int are compatible in most situations
 answer = "True"
 answer = bool(answer) #converts any string except 0 or blank to boolean True
 print(answer)
-#if variable is formatted correctly when declared it will automatically
+#if variable is formatted correctly when declared it will automatically assign the correct
 # format type. Values from input are always strings.
